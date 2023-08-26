3245b98d19cb1c8ab6dffa0e228e5a7d2aa796d2
Erick Santana <86210226+classdeveloper@users.noreply.github.com>
2023-08-26 19:53:56 -0300

    Add files via upload

--- home.java
+++ home.java
@@ -1,147 +1,159 @@
+package bank;
+
 import java.util.Scanner;
 
 public class home {
 
-    private static void cadastro() {
-
-        Scanner entrada = new Scanner(System.in);
-        variaveis variaveis = new variaveis();
-
-        System.out.println("1) Registro \n2) Login");
-
-        System.out.print("\nSeletor: ");
-        variaveis.seletorRegistro = entrada.nextInt();
-
-        switch (variaveis.seletorRegistro) {
-
-            case 1:
-
-                System.out.println("Digite seu nome: ");
-                variaveis.nome = entrada.next();
-                System.out.println("Digite seu sobrenome: ");
-                variaveis.sobrenome = entrada.next();
-                System.out.println("Digite sua idade: ");
-                variaveis.idade = entrada.nextInt();
-                System.out.println("Digite seu e-mail: ");
-                variaveis.emailRegistro = entrada.next();
-                System.out.println("Digite seu CPF: ");
-                variaveis.CPFRegistro = entrada.next();
-                System.out.println("Digite sua senha: ");
-                variaveis.senhaRegistro = entrada.next();
-                System.out.println("Digite sua senha novamente: ");
-                variaveis.confirmsenhaRegistro = entrada.next();
-
-                System.out.println("Deseja ativar sua conta? ");
-                variaveis.ativarRegistro = entrada.next();
-
-                if (!variaveis.confirmsenhaRegistro.equals(variaveis.senhaRegistro)) {
-
-                    System.out.println("As senhas não batem se cadastre novamente!");
-                }
-
-                else if (variaveis.idade < 18) {
-
-                    System.out.println("Você não tem idade suficiente!");
-
-                }
-
-                else {
-
-                    System.out.println("Você criou sua conta com sucesso!");
-
-                }
-
-                break;
-
-            case 2:
-
-                System.out.println("Digite seu e-mail: ");
-                variaveis.emailLogin = entrada.next();
-                System.out.println("Digite seu CPF: ");
-                variaveis.CPFLogin = entrada.next();
-                System.out.println("Digite sua senha: ");
-                variaveis.senhaLogin = entrada.next();
-                System.out.println("Digite sua senha novamente: ");
-                variaveis.confirmsenhaLogin = entrada.next();
-
-                System.out.println("Deseja entrar em sua conta? ");
-                variaveis.ativarLogin = entrada.next();
-
-                if (!variaveis.emailLogin.equals(variaveis.emailRegistro) && !variaveis.CPFLogin.equals(variaveis.CPFRegistro) && !variaveis.senhaLogin.equals(variaveis.senhaRegistro)) {
-
-                    System.out.println("Suas credênciais estão erradas!");
-
-                }
-
-                else {
-
-                    System.out.println("Seja bem-vindo novamente!");
-
-                }
-                break;
-
-        }
-
-        entrada.close();
-    }
-
-    public static void painel() {
-
-        variaveis variaveis = new variaveis();
-        Scanner entrada = new Scanner (System.in);
-
-        System.out.println("PAINEL: ");
-        System.out.println("1) Minhas informações");
-        System.out.println("2) Transferências");
-        System.out.println("3) Saldo");
-        System.out.println("4) Deposito");
-        variaveis.seletorPainel = entrada.nextInt();
-
-        switch (variaveis.seletorPainel) {
-
-            case 1:
-
-                System.out.println("Nome: " + variaveis.nome + " " + variaveis.sobrenome);
-                System.out.println("Email: " + variaveis.emailRegistro);
-                System.out.println("CPF: " + variaveis.CPFRegistro);
-                System.out.println("Idade: " + variaveis.idade);
-                break;
-
-            case 2:
-
-                System.out.println("Digite o CPF: ");
-                variaveis.CPFTransferencias = entrada.next();
-                System.out.println("Digite o valor: ");
-                variaveis.valor = entrada.nextInt();
-                break;
-
-            case 3:
-                //System.out.println("Seu saldo total é: " + variaveis.saldoTotal);
-                break;
-
-            case 4:
-                System.out.println("Digite o valor que deseja depositar: ");
-                variaveis.deposito = entrada.nextDouble();
-
-        }
-
-    entrada.close();
-
-    }
-
-    public static void main(String[] args) {
-
-        variaveis variaveis = new variaveis();
-
-       cadastro();
-
-       if (variaveis.ativarRegistro.equals("sim") || variaveis.ativarLogin.equals("sim")) {
-
-           painel();
-
-       }
-
-
-
-    }
-}
+	public static void main(String[] args) {
+		
+		boolean retornar = false;
+		
+		do {
+		
+		Scanner entrada = new Scanner (System.in);
+			
+		System.out.println("1) Register");
+		System.out.println("2) Login");
+		System.out.print("Selecionar: ");
+		int controlador = entrada.nextInt();
+		
+		switch (controlador) {
+		
+		case 1:
+			
+			System.out.println("Seja bem-vindo a página de registro!");
+			
+			System.out.print("Digite seu nome: ");
+			variables.name = entrada.next();
+			System.out.print("Digite seu sobrenome: ");
+			variables.sobrenome = entrada.next();
+			System.out.print("Digite seu e-mail: ");
+			variables.email = entrada.next();
+			System.out.print("Digite seu CPF: ");
+			variables.CPF = entrada.next();
+			System.out.print("Digite sua senha: ");
+			variables.senha = entrada.next();
+			System.out.print("Digite sua senha novamente: ");
+			variables.repsenha = entrada.next();
+			
+			break;
+			
+		case 2:
+			
+			System.out.println("Seja bem-vindo novamente a página de login!");
+			
+			System.out.print("Digite seu e-mail ou CPF: ");
+			variables.name = entrada.next();
+			
+			System.out.print("Digite sua senha: ");
+			variables.senha = entrada.next();
+			
+			System.out.print("Digite sua senha novamente: ");
+			variables.repsenha = entrada.next();
+			
+			if (variables.senha.equals(variables.repsenha)) {
+				
+				System.out.print("Seja bem-vindo novamente!");
+			}
+			
+			else {
+				
+				System.out.println("Suas credenciais estão erradas!");
+			}
+			break;
+		}
+		
+		
+		System.out.println("\nPAINEL DE INFORMAÇÕES");
+		
+		System.out.println("1) Dados");
+		System.out.println("2) Saldo");
+		System.out.println("3) Depositar");
+		int controlador2 = entrada.nextInt();
+		
+		switch (controlador2) {
+		
+		case 1: 
+			
+			System.out.println("Nome completo: " + variables.name + " " + variables.sobrenome);
+			System.out.println("Email: " + variables.email);
+			System.out.println("CPF: " + variables.CPF);
+			
+			retornar = true;
+			
+			break; 
+			
+		case 2:
+			
+			System.out.println("Seu saldo atual é: " + variables.saldo);
+			
+			retornar = true;
+			
+			break;
+			
+		case 3: 
+			
+			System.out.println("PAINEL DEPOSITO");
+			
+			System.out.println("1) PIX");
+			System.out.println("2) Boleto");
+			System.out.print("Seletor: ");
+			int controlador3 = entrada.nextInt();
+			
+			
+			switch (controlador3) {
+			
+			case 1: 
+				
+				System.out.print("Insira um CPF: ");
+				variables.PIX = entrada.next();
+				
+				System.out.println("Deseja mesmo realizar essa transferencia? ");
+				variables.confirmacaoPIX = entrada.next();
+				
+				if (variables.confirmacaoPIX.equalsIgnoreCase("sim")) {
+					
+					System.out.println("Transferencia realizada com sucesso!");
+					
+					retornar = true;
+					
+				}
+				
+				else {
+					
+					System.out.println("Até mais!");
+					
+					retornar = true;
+					
+				}
+				
+				break;
+				
+			case 2:
+				
+				System.out.println("Insira o código: ");
+				variables.boleto = entrada.next();
+				
+				System.out.println("Deseja mesmo realizar essa transferencia? ");
+				variables.confirmacaoBOLETO = entrada.next();
+				
+				if (variables.confirmacaoBOLETO.equalsIgnoreCase("Sim")) {
+					
+					System.out.println("Transferencia realizada com sucesso!");
+					
+					retornar = true;
+					
+				}
+				
+				
+				break; 
+			}
+		}
+		
+	}while (retornar);
+		
+		
+	}
+	
+	}
+	
---
+++ variables.java
@@ -0,0 +1,18 @@
+package bank;
+
+public class variables {
+	
+	public static String name;
+	public static String sobrenome;
+	public static String email;
+	public static String CPF;
+	public static String senha;
+	public static String repsenha;
+	public static double saldo;
+	public static String PIX;
+	public static String confirmacaoPIX;
+	public static String confirmacaoBOLETO;
+	public static String boleto;
+	public static String retornar;
+	
+}
